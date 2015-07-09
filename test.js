067c2a57e640c53d045e36e33e19bdf2429ef4b3
MoreFreeze <liuchenxing@acfun.tv>
2015-07-09 19:14:14 +0800

    add dkbson.js and test file

---
+++ test.js
@@ -0,0 +1,4 @@
+var dk = require("./dkbson");
+console.log(dk);
+res = dk.dkbson.decode('AiBzdGF0dXMAAm9rIHVybABQaHR0cDovL3BhZ2VzLnJlYWQuZHVva2FuLmNvbS9tZnN2Mi9kb3dubG9hZC9zMDEwL3AwMVQ4T2FnU3F3SS9KcXd3Wmp5Y2syWmxlS3QuanM=');
+console.log(res);
