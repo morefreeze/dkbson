a66b8299a14242717daaf0f7531afae1e5bc941e
刘晨星 <liuchenxing@acfun.tv>
2015-08-07 11:54:08 +0800

    add missing semicolon

--- dkbson.js
+++ dkbson.js
@@ -26,88 +26,89 @@ with (p.encodeFloat = function(t, e, i) {
         ;
     for (v = 0, a = 0, n = (l = (m ? "1" : "0") + l + _.slice(n, n + e).join("")).length, r = []; n; v += (1 << a) * l.charAt(--n), 7 == a && (r[r.length] = String.fromCharCode(v), v = 0), a = (a + 1) % 8)
         ;
-    return r[r.length] = v ? String.fromCharCode(v) : "", (this.bigEndian ? r.reverse() : r).join("")
+    return r[r.length] = v ? String.fromCharCode(v) : "", (this.bigEndian ? r.reverse() : r).join("");
 }, p.encodeInt = function(t, e) {
     var i = Math.pow(2, e), n = [];
     for ((t >= i || -(i >> 1) > t) && this.warn("encodeInt::overflow") && (t = 0), 0 > t && (t += i); t; n[n.length] = String.fromCharCode(t % 256), t = Math.floor(t / 256))
         ;
     for (e = -(-e >> 3) - n.length; e--; n[n.length] = "\0")
         ;
-    return (this.bigEndian ? n.reverse() : n).join("")
+    return (this.bigEndian ? n.reverse() : n).join("");
 }, p.decodeFloat = function(t, e, i) {
     var n, r, o, s = ((s = new this.Buffer(this.bigEndian, t)).checkBuffer(e + i + 1), s), a = Math.pow(2, i - 1) - 1, l = s.readBits(e + i, 1), c = s.readBits(e, i), u = 0, h = 2, d = s.buffer.length + (-e >> 3) - 1;
-    do
+    do{
         for (n = s.buffer[++d], r = e % 8 || 8, o = 1 << r; o >>= 1; n & o && (u += 1 / h), h *= 2)
             ;
-    while (e -= r);
-    return c == (a << 1) + 1 ? u ? 0 / 0 : l ? -1 / 0 : +1 / 0 : (1 + -2 * l) * (c || u ? c ? Math.pow(2, c - a) * (1 + u) : Math.pow(2, -a + 1) * u : 0)
+        e -= r;
+    }while (e);
+    return c == (a << 1) + 1 ? u ? 0 / 0 : l ? -1 / 0 : +1 / 0 : (1 + -2 * l) * (c || u ? c ? Math.pow(2, c - a) * (1 + u) : Math.pow(2, -a + 1) * u : 0);
 }, p.decodeInt = function(t, e, i) {
     var n = new this.Buffer(this.bigEndian, t), r = n.readBits(0, e), o = Math.pow(2, e);
-    return i && r >= o / 2 ? r - o : r
+    return i && r >= o / 2 ? r - o : r;
 }, {p: (p.Buffer = function(t, e) {
-        this.bigEndian = t || 0, this.buffer = [], this.setBuffer(e)
+        this.bigEndian = t || 0, this.buffer = [], this.setBuffer(e);
     }).prototype})
     p.readBits = function(t, e) {
         function i(t, e) {
             for (++e; --e; t = 1073741824 == (1073741824 & (t %= 2147483648)) ? 2 * t : 2 * (t - 1073741824) + 2147483647 + 1)
                 ;
-            return t
+            return t;
         }
         if (0 > t || 0 >= e)
             return 0;
         this.checkBuffer(t + e);
         for (var n, r = t % 8, o = this.buffer.length - (t >> 3) - 1, s = this.buffer.length + (-(t + e) >> 3), a = o - s, l = (this.buffer[o] >> r & (1 << (a ? 8 - r : e)) - 1) + (a && (n = (t + e) % 8) ? (this.buffer[s++] & (1 << n) - 1) << (a-- << 3) - r : 0); a; l += i(this.buffer[s++], (a-- << 3) - r))
             ;
-        return l
+        return l;
     }, p.setBuffer = function(t) {
         if (t) {
             for (var e, i = e = t.length, n = this.buffer = Array(e); i; n[e - i] = t.charCodeAt(--i))
                 ;
-            this.bigEndian && n.reverse()
+            this.bigEndian && n.reverse();
         }
     }, p.hasNeededBits = function(t) {
-        return this.buffer.length >= -(-t >> 3)
+        return this.buffer.length >= -(-t >> 3);
     }, p.checkBuffer = function(t) {
         if (!this.hasNeededBits(t))
-            throw Error("checkBuffer::missing bytes")
+            throw Error("checkBuffer::missing bytes");
     };
 p.warn = function(t) {
     if (this.allowExceptions)
         throw Error(t);
-    return 1
+    return 1;
 }, p.toSmall = function(t) {
-    return this.decodeInt(t, 8, !0)
+    return this.decodeInt(t, 8, !0);
 }, p.fromSmall = function(t) {
-    return this.encodeInt(t, 8, !0)
+    return this.encodeInt(t, 8, !0);
 }, p.toByte = function(t) {
-    return this.decodeInt(t, 8, !1)
+    return this.decodeInt(t, 8, !1);
 }, p.fromByte = function(t) {
-    return this.encodeInt(t, 8, !1)
+    return this.encodeInt(t, 8, !1);
 }, p.toShort = function(t) {
-    return this.decodeInt(t, 16, !0)
+    return this.decodeInt(t, 16, !0);
 }, p.fromShort = function(t) {
-    return this.encodeInt(t, 16, !0)
+    return this.encodeInt(t, 16, !0);
 }, p.toWord = function(t) {
-    return this.decodeInt(t, 16, !1)
+    return this.decodeInt(t, 16, !1);
 }, p.fromWord = function(t) {
-    return this.encodeInt(t, 16, !1)
+    return this.encodeInt(t, 16, !1);
 }, p.toInt = function(t) {
-    return this.decodeInt(t, 32, !0)
+    return this.decodeInt(t, 32, !0);
 }, p.fromInt = function(t) {
-    return this.encodeInt(t, 32, !0)
+    return this.encodeInt(t, 32, !0);
 }, p.toDWord = function(t) {
-    return this.decodeInt(t, 32, !1)
+    return this.decodeInt(t, 32, !1);
 }, p.fromDWord = function(t) {
-    return this.encodeInt(t, 32, !1)
+    return this.encodeInt(t, 32, !1);
 }, p.toFloat = function(t) {
-    return this.decodeFloat(t, 23, 8)
+    return this.decodeFloat(t, 23, 8);
 }, p.fromFloat = function(t) {
-    return this.encodeFloat(t, 23, 8)
+    return this.encodeFloat(t, 23, 8);
 }, p.toDouble = function(t) {
-    return this.decodeFloat(t, 52, 11)
+    return this.decodeFloat(t, 52, 11);
 }, p.fromDouble = function(t) {
-    return this.encodeFloat(t, 52, 11)
-}
+    return this.encodeFloat(t, 52, 11);
+};
 base64 = function() {
         function t(t, e) {
             var i = o.indexOf(t.charAt(e));
@@ -128,7 +129,7 @@ base64 = function() {
                     n = t(e, i) << 18 | t(e, i + 1) << 12 | t(e, i + 2) << 6, a.push(String.fromCharCode(n >> 16, 255 & n >> 8));
                     break;
                 case 2:
-                    n = t(e, i) << 18 | t(e, i + 1) << 12, a.push(String.fromCharCode(n >> 16))
+                    n = t(e, i) << 18 | t(e, i + 1) << 12, a.push(String.fromCharCode(n >> 16));
             }
             return a.join("");
         }
@@ -152,7 +153,7 @@ base64 = function() {
                     n = i(t, e) << 16, s.push(o.charAt(n >> 18) + o.charAt(63 & n >> 12) + r + r);
                     break;
                 case 2:
-                    n = i(t, e) << 16 | i(t, e + 1) << 8, s.push(o.charAt(n >> 18) + o.charAt(63 & n >> 12) + o.charAt(63 & n >> 6) + r)
+                    n = i(t, e) << 16 | i(t, e + 1) << 8, s.push(o.charAt(n >> 18) + o.charAt(63 & n >> 12) + o.charAt(63 & n >> 6) + r);
             }
             return s.join("");
         }
@@ -175,7 +176,7 @@ dkbson = function() {
             return [e + i, a];
         }
         function i(i, n, r) {
-            var o, s = new BinaryParser;
+            var o, s = new BinaryParser();
             if (i == l.TYPE_INT8)
                 o = s.toSmall(n.substr(r, 1)), r++;
             else if (i == l.TYPE_INT16)
@@ -193,7 +194,7 @@ dkbson = function() {
                 var a = e(n, r);
                 r = a[0];
                 var c = a[1];
-                o = n.substr(r, c), o = t(o), r += c
+                o = n.substr(r, c), o = t(o), r += c;
             } else
                 i == l.TYPE_NULL ? (o = null, r++) : i == l.TYPE_BOOL ? (o = 0 === n.charCodeAt(r) ? !1 : !0, r++) : i == l.TYPE_REAL16 ? (o = s.toShort(n.substr(r, 2)), o /= 100, r += 2) : i == l.TYPE_REAL24 ? (o = s.toInt(n.substr(r, 3) + "\0"), o /= 1e3, r += 3) : i == l.TYPE_REAL32 ? (o = s.toInt(n.substr(r, 4)), o /= 1e4, r += 4) : console.log("error: unsupported type:" + i.charCodeAt(0));
             return [r, o];
@@ -203,7 +204,7 @@ dkbson = function() {
             o = a[0];
             for (var c = a[1], u = c; u-- > 0; ) {
                 var h, d, f = t.charCodeAt(o), p = o + 1;
-                f == l.TYPE_OBJECT ? (h = r(t, p), o = h[0], d = h[1]) : f == l.TYPE_ARRAY ? (h = n(t, p), o = h[0], d = h[1]) : (h = i(f, t, p), o = h[0], d = h[1]), s.push(d)
+                f == l.TYPE_OBJECT ? (h = r(t, p), o = h[0], d = h[1]) : f == l.TYPE_ARRAY ? (h = n(t, p), o = h[0], d = h[1]) : (h = i(f, t, p), o = h[0], d = h[1]), s.push(d);
             }
             return [o, s];
         }
